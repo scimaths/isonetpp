d3d77c4d75cc24581095e17cbfe8b3629cad4c45
Vaibhav Raj <mevaibhav045@gmail.com>
2023-12-17 17:16:51 +0530

    Updated run_experiments

--- run_experiments.sh
+++ run_experiments.sh
@@ -29,7 +29,7 @@ datasets=('aids' 'mutag' 'ptc_fr' 'ptc_fm' 'ptc_mr' 'ptc_mm')
 for ((idx=0; idx<${#datasets[@]}; idx++)); do
     dataset="${datasets[$idx]}"
     for seed in 0 1 2 3 4; do
-        CUDA_VISIBLE_DEVICES=$(((idx * 5 + seed) % 4)) python -m subgraph.iso_matching_models \
+        CUDA_VISIBLE_DEVICES=$(((idx * 6 + seed) % 4)) python -m subgraph.iso_matching_models \
         --experiment_group=${experiment_group} \
         --TASK=${TASK} \
         --time_updates=${time_updates} \
