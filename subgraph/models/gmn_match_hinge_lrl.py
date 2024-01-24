1e4361c618e8273b91a6c4f73cae799116bdf147
AshwinRamachandran2002 <200050016@iitb.ac.in>
2024-01-25 03:42:26 +0530

    adding inter script to equal node interaction baseline to gmn lrl inter

--- subgraph/models/gmn_match_hinge_lrl.py
+++ subgraph/models/gmn_match_hinge_lrl.py
@@ -16,7 +16,6 @@ class CrossAttention(torch.nn.Module):
         self.dim = dim
         self.injective_attention = injective_attention
         self.use_sinkhorn = use_sinkhorn
-        self.attention_to_sinkhorn = attention_to_sinkhorn
         self.max_node_size = max_node_size
         if not self.use_sinkhorn:
             self.max_node_size = self.max_node_size + 1
