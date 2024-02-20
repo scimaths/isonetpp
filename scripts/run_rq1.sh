0b897a6b3d9591ff89a83a47df259674beccf396
Vaibhav Raj <mevaibhav045@gmail.com>
2024-02-21 04:28:27 +0530

    Added configs and script for RQ1

---
+++ scripts/run_rq1.sh
@@ -0,0 +1,37 @@
+cd ..
+
+declare -A dataset_seeds=(
+   ["aids"]="356"
+   ["mutag"]="1704"
+   ["ptc_fm"]="3133"
+   ["ptc_fr"]="4929"
+   ["ptc_mm"]="7366"
+   ["ptc_mr"]="7762"
+)
+
+gpus=(0 1 5 6 7)
+config_counter=0
+
+for config_file in \
+   "configs/gmn_baselines/rq1/scoring=agg___tp=masked_attention_pp=lrl_when=post.yaml" \
+   "configs/gmn_baselines/rq1/scoring=attention_pp=lrl___tp=masked_attention_pp=lrl_when=post.yaml" \
+   "configs/gmn_baselines/rq1/scoring=masked_attention_pp=lrl___tp=masked_attention_pp=lrl_when=post.yaml" \
+   "configs/gmn_baselines/rq1/scoring=sinkhorn_pp=lrl___tp=masked_attention_pp=lrl_when=post.yaml" \
+; do
+   dataset_counter=0
+   for dataset in "${!dataset_seeds[@]}"; do
+      seed="${dataset_seeds[$dataset]}"
+      gpu_index=$(( (dataset_counter + config_counter) % ${#gpus[@]} ))
+
+      echo """CUDA_VISIBLE_DEVICES=${gpus[$gpu_index]} python3 -m subgraph_matching.train \
+         --experiment_id rq1 \
+         --experiment_dir experiments/ \
+         --model_config_path $config_file \
+         --dataset_name $dataset \
+         --seed $seed \
+         --dataset_size large"""
+   
+      ((dataset_counter++))
+   done
+   ((config_counter++))
+done
