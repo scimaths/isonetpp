3e7289ce207778c806d61c590545263fc8f19735
Vaibhav Raj <mevaibhav045@gmail.com>
2024-02-25 16:33:54 +0530

    added rq2 configs and script

---
+++ scripts/run_rq2.sh
@@ -0,0 +1,39 @@
+cd ..
+
+declare -A dataset_seeds=(
+   ["aids"]="356"
+   ["mutag"]="1704"
+   ["ptc_fm"]="3133"
+   ["ptc_fr"]="4929"
+   ["ptc_mm"]="7366"
+   ["ptc_mr"]="7762"
+)
+
+gpus=(0 1 2 3)
+overall_counter=0
+
+for config_file in \
+   "configs/rq2/scoring=sinkhorn_pp=lrl___tp=sinkhorn_pp=lrl_when=post.yaml" \
+   "configs/rq2/scoring=attention_pp=lrl___tp=masked_attention_pp=lrl_when=post.yaml" \
+   "configs/rq2/scoring=sinkhorn_pp=lrl___tp=masked_attention_pp=lrl_when=post.yaml" \
+; do
+   # "configs/rq2/scoring=attention_pp=lrl___tp=attention_pp=lrl_when=post.yaml" \
+   # "configs/rq2/scoring=attention_pp=lrl___tp=sinkhorn_pp=lrl_when=post.yaml" \
+   # "configs/rq2/scoring=sinkhorn_pp=lrl___tp=attention_pp=lrl_when=post.yaml" \
+   for dataset in "${!dataset_seeds[@]}"; do
+      seed="${dataset_seeds[$dataset]}"
+      gpu_index=$(( (overall_counter) % ${#gpus[@]} ))
+
+      CUDA_VISIBLE_DEVICES=${gpus[$gpu_index]} python3 -m subgraph_matching.train \
+         --experiment_id rq2 \
+         --experiment_dir experiments/ \
+         --model_config_path $config_file \
+         --dataset_name $dataset \
+         --seed $seed \
+         --dataset_size large \
+         &
+
+      ((overall_counter++))
+      sleep 30s
+   done
+done
