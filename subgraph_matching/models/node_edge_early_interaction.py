affaee0309b0ce9d6202269b5e7cca7ee11d71d3
AshwinRamachandran2002 <200050016@iitb.ac.in>
2024-04-06 06:46:13 +0530

    refactoring pending for node edge

--- subgraph_matching/models/node_edge_early_interaction.py
+++ subgraph_matching/models/node_edge_early_interaction.py
@@ -25,12 +25,8 @@ class NodeEdgeEarlyInteraction(torch.nn.Module):
         self.max_edge_set_size = max_edge_set_size
         self.device = device
 
-        self.node_graph_size_to_mask_map = model_utils.graph_size_to_mask_map(
+        self.graph_size_to_mask_map = model_utils.graph_size_to_mask_map(
             max_set_size=max_node_set_size, lateral_dim=sinkhorn_feature_dim, device=self.device
-        )
-
-        self.edge_graph_size_to_mask_map = model_utils.graph_size_to_mask_map(
-            max_set_size=max_edge_set_size, lateral_dim=sinkhorn_feature_dim, device=self.device
         )
 
         self.encoder = gmngen.GraphEncoder(**encoder_config)
@@ -47,7 +43,7 @@ class NodeEdgeEarlyInteraction(torch.nn.Module):
         )
 
         self.sinkhorn_config = sinkhorn_config
-        self.node_sinkhorn_feature_layers = torch.nn.Sequential(
+        self.sinkhorn_feature_layers = torch.nn.Sequential(
             torch.nn.Linear(propagation_layer_config.node_state_dim, sinkhorn_feature_dim),
             torch.nn.ReLU(),
             torch.nn.Linear(sinkhorn_feature_dim, sinkhorn_feature_dim)
@@ -60,12 +56,6 @@ class NodeEdgeEarlyInteraction(torch.nn.Module):
             torch.nn.Linear(interaction_input_dim, interaction_input_dim),
             torch.nn.ReLU(),
             torch.nn.Linear(interaction_input_dim, interaction_output_dim)
-        )
-
-        self.edge_sinkhorn_feature_layers = torch.nn.Sequential(
-            torch.nn.Linear(self.message_dim, sinkhorn_feature_dim),
-            torch.nn.ReLU(),
-            torch.nn.Linear(sinkhorn_feature_dim, sinkhorn_feature_dim)
         )
 
         self.consistency_config = consistency_config
@@ -106,108 +96,89 @@ class NodeEdgeEarlyInteraction(torch.nn.Module):
         for _ in range(self.time_update_steps):
             node_features_enc, edge_features_enc = torch.clone(encoded_node_features), torch.clone(encoded_edge_features)
 
+            interaction_features = node_feature_store[:, :node_feature_dim]
+            node_combined_features = self.node_interaction_encoder(torch.cat([node_features_enc, interaction_features], dim=-1))
+
             for prop_idx in range(1, self.propagation_steps + 1):
-                # Combine interaction features with node features from previous propagation step
+                edge_interaction_idx = self.message_dim * prop_idx
                 node_interaction_idx = node_feature_dim * prop_idx
-                interaction_features = node_feature_store[:, node_interaction_idx - node_feature_dim : node_interaction_idx]
-                node_combined_features = self.node_interaction_encoder(torch.cat([node_features_enc, interaction_features], dim=-1))
 
                 # Combine interaction features with edge features from previous propagation step
-                edge_interaction_idx = self.message_dim * prop_idx
                 interaction_features = edge_feature_store[:, edge_interaction_idx - self.message_dim : edge_interaction_idx]
                 edge_combined_features = self.edge_interaction_encoder(torch.cat([edge_features_enc, interaction_features], dim=-1))
 
                 # Message propagation on combined features
                 node_features_enc = self.prop_layer(node_combined_features, from_idx, to_idx, edge_combined_features)
 
+                # Combine interaction features with node features from previous propagation step
+                interaction_features = node_feature_store[:, node_interaction_idx : node_interaction_idx + node_feature_dim]
+                node_combined_features = self.node_interaction_encoder(torch.cat([node_features_enc, interaction_features], dim=-1))
                 updated_node_feature_store[:, node_interaction_idx : node_interaction_idx + node_feature_dim] = torch.clone(node_features_enc)
 
                 messages = model_utils.propagation_messages(
                     propagation_layer=self.prop_layer,
-                    node_features=node_features_enc,
+                    node_features=node_combined_features,
                     edge_features=edge_combined_features,
                     from_idx=from_idx,
                     to_idx=to_idx
                 )
                 updated_edge_feature_store[:, edge_interaction_idx : edge_interaction_idx + self.message_dim] = torch.clone(messages)
 
-            def compute_transport_plan(
-                updated_feature_store,
-                sizes,
-                max_set_size,
-                feature_dim,
-                sinkhorn_feature_layers,
-                graph_size_to_mask_map
-            ):
-                feature_store = torch.clone(updated_feature_store)
-                stacked_feature_store_query, stacked_feature_store_corpus = model_utils.split_and_stack(
-                    feature_store, sizes, max_set_size
-                )
-
-                # Compute node transport plan
-                final_features_query = stacked_feature_store_query[:, :, -feature_dim:]
-                final_features_corpus = stacked_feature_store_corpus[:, :, -feature_dim:]
-
-                transformed_features_query = sinkhorn_feature_layers(final_features_query)
-                transformed_features_corpus = sinkhorn_feature_layers(final_features_corpus)
-
-                def mask_graphs(features, graph_sizes):
-                    mask = torch.stack([graph_size_to_mask_map[i] for i in graph_sizes])
-                    return mask * features
-                masked_features_query = mask_graphs(transformed_features_query, query_sizes)
-                masked_features_corpus = mask_graphs(transformed_features_corpus, corpus_sizes)
-
-                sinkhorn_input = torch.matmul(masked_features_query, masked_features_corpus.permute(0, 2, 1))
-                transport_plan = model_utils.sinkhorn_iters(log_alpha=sinkhorn_input, device=self.device, **self.sinkhorn_config)
-                interleaved_features = model_utils.get_interaction_feature_store(
-                    transport_plan, stacked_feature_store_query, stacked_feature_store_corpus
-                )
-
-                return (
-                    transport_plan,
-                    interleaved_features,
-                    final_features_query,
-                    final_features_corpus
-                )
-
-
-            
-            (
-                node_transport_plan,
-                interleaved_node_features,
-                final_features_query,
-                final_features_corpus
-            ) = compute_transport_plan(
-                updated_node_feature_store,
-                graph_sizes,
-                self.max_node_set_size,
-                node_feature_dim,
-                self.node_sinkhorn_feature_layers,
-                self.node_graph_size_to_mask_map
+            node_feature_store = torch.clone(updated_node_feature_store)
+            stacked_feature_store_query, stacked_feature_store_corpus = model_utils.split_and_stack(
+                node_feature_store, graph_sizes, self.max_node_set_size
             )
 
-            _, interleaved_edge_features, _, _ = compute_transport_plan(
-                updated_edge_feature_store,
-                paired_edge_counts,
-                self.max_edge_set_size,
-                self.message_dim,
-                self.edge_sinkhorn_feature_layers,
-                self.edge_graph_size_to_mask_map
-            )
+            # Compute node transport plan
+            final_features_query = stacked_feature_store_query[:, :, -node_feature_dim:]
+            final_features_corpus = stacked_feature_store_corpus[:, :, -node_feature_dim:]
 
+            transformed_features_query = self.sinkhorn_feature_layers(final_features_query)
+            transformed_features_corpus = self.sinkhorn_feature_layers(final_features_corpus)
+
+            def mask_graphs(features, graph_sizes):
+                mask = torch.stack([self.graph_size_to_mask_map[i] for i in graph_sizes])
+                return mask * features
+            masked_features_query = mask_graphs(transformed_features_query, query_sizes)
+            masked_features_corpus = mask_graphs(transformed_features_corpus, corpus_sizes)
+
+            sinkhorn_input = torch.matmul(masked_features_query, masked_features_corpus.permute(0, 2, 1))
+            node_transport_plan = model_utils.sinkhorn_iters(log_alpha=sinkhorn_input, device=self.device, **self.sinkhorn_config)
+
+            # Compute interaction-based features
+            interleaved_node_features = model_utils.get_interaction_feature_store(
+                node_transport_plan, stacked_feature_store_query, stacked_feature_store_corpus
+            )
             node_feature_store[:, node_feature_dim:] = interleaved_node_features[padded_node_indices, node_feature_dim:]
+
+
+            straight_mapped_scores, cross_mapped_scores = model_utils.kronecker_product_on_nodes(
+                node_transport_plan=node_transport_plan, from_idx=from_idx,
+                to_idx=to_idx, paired_edge_counts=paired_edge_counts,
+                graph_sizes=graph_sizes, max_edge_set_size=self.max_edge_set_size
+            )
+
+            edge_transport_plan = model_utils.sinkhorn_iters(
+                log_alpha=straight_mapped_scores+cross_mapped_scores,
+                device=self.device, **self.sinkhorn_config
+            )
+
+            edge_feature_store = torch.clone(updated_edge_feature_store)
+            stacked_feature_store_query, stacked_feature_store_corpus = model_utils.split_and_stack(
+                edge_feature_store, paired_edge_counts, self.max_edge_set_size
+            )
+
+            # Compute node transport plan
+            final_edge_features_query = stacked_feature_store_query[:, :, -self.message_dim:]
+            final_edge_features_corpus = stacked_feature_store_corpus[:, :, -self.message_dim:]
+
+            # Compute interaction-based features
+            interleaved_edge_features = model_utils.get_interaction_feature_store(
+                edge_transport_plan, stacked_feature_store_query, stacked_feature_store_corpus
+            )
             edge_feature_store[:, self.message_dim:] = interleaved_edge_features[padded_edge_indices, self.message_dim:]
 
         score = model_utils.feature_alignment_score(final_features_query, final_features_corpus, node_transport_plan)
+        consistency = self.consistency_config.consistency_weight * model_utils.feature_alignment_score(final_edge_features_query, final_edge_features_corpus, edge_transport_plan)
 
-        if self.consistency_config:
-            return torch.add(
-                score,
-                self.consistency_score(
-                    graphs,
-                    graph_sizes,
-                    messages,
-                    node_transport_plan
-                )
-            )
         return score
