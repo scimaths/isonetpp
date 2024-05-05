69edbc244b797e5ac4f584f951097176b7aaada1
AshwinRamachandran2002 <200050016@iitb.ac.in>
2024-05-06 04:47:06 +0530

    crap baseliens config dgx

--- subgraph_matching/model_handler.py
+++ subgraph_matching/model_handler.py
@@ -18,13 +18,11 @@ from subgraph_matching.models.graphsim import GraphSim
 from subgraph_matching.models.egsc_modified import EGSC as EGSC_Modified
 from subgraph_matching.models.egsc import EGSC
 from subgraph_matching.models.eric import ERIC
-<<<<<<< HEAD
 from subgraph_matching.models.gotsim import GOTSim
 from subgraph_matching.models.gmn_embed import GMN_embed_hinge
-=======
 from subgraph_matching.models.h2mn import H2MN
 from subgraph_matching.models.greed import Greed
->>>>>>> cb0a29b4349db3a78cc8a8ade7d4f6f3d6ed04cc
+from subgraph_matching.models.neuromatch import NeuroMatch
 
 model_name_to_class_mappings = {
     'node_align_node_loss': NodeAlignNodeLoss,
@@ -54,8 +52,9 @@ model_name_to_class_mappings = {
     'egsc_modified': EGSC_Modified,
     'eric': ERIC,
     'gotsim': GOTSim,
-    'gmn_embed': GMN_embed_hinge
+    'gmn_embed': GMN_embed_hinge,
     'H2MN': H2MN,
+    'neuromatch': NeuroMatch,
     'greed': Greed
 }
 
@@ -78,6 +77,6 @@ def get_model(model_name, config, max_node_set_size, max_edge_set_size, device):
     )
 
 def get_data_type_for_model(model_name):
-    if model_name in ['graphsim', 'egsc', 'egsc_modified', 'eric', 'gotsim', 'H2MN', 'greed']:
+    if model_name in ['graphsim', 'egsc', 'egsc_modified', 'eric', 'gotsim', 'H2MN', 'greed', 'neuromatch']:
         return dataset.PYG_DATA_TYPE
     return dataset.GMN_DATA_TYPE
